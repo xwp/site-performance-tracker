0d5845496d578480ebe43f6fa916e46a0fa3b754
Mike Crantea <mike.crantea@xwp.co>
2021-04-08 12:56:26 +0100

    Format: Lint JS

--- js/src/web-vitals-analytics.js
+++ js/src/web-vitals-analytics.js
@@ -14,165 +14,169 @@
  * limitations under the License.
  */
 
-/* global gtag */
+/* global gtag, location, requestIdleCallback */
 
-import { getCLS, getFCP, getFID, getLCP } from "web-vitals";
+import { getCLS, getFCP, getFID, getLCP } from 'web-vitals';
 
-const getConfig = (id) => {
+const getConfig = id => {
 	const config = {
-		measurement_version: "4",
+		measurement_version: '4',
 		page_path: location.pathname,
 	};
 
-	if (id.startsWith("UA-")) {
-		Object.assign(config, {
-			transport_type: "beacon",
+	if ( id.startsWith( 'UA-' ) ) {
+		Object.assign( config, {
+			transport_type: 'beacon',
 			custom_map: {
-				dimension1: "measurement_version",
-				dimension2: "client_id",
-				dimension3: "segments",
-				dimension4: "config",
-				dimension5: "event_meta",
-				dimension6: "event_debug",
-				metric1: "report_size",
+				dimension1: 'measurement_version',
+				dimension2: 'client_id',
+				dimension3: 'segments',
+				dimension4: 'config',
+				dimension5: 'event_meta',
+				dimension6: 'event_debug',
+				metric1: 'report_size',
 			},
-		});
+		} );
 	}
-	if (id.startsWith("G-")) {
-		if (location.hostname !== "web-vitals-report.web.app") {
+	if ( id.startsWith( 'G-' ) ) {
+		if ( location.hostname !== 'web-vitals-report.web.app' ) {
 			config.debug_mode = true;
 		}
 	}
-	return ["config", id, config];
+	return [ 'config', id, config ];
 };
 
-const thresholds = {
-	CLS: [0.1, 0.25],
-	FCP: [1800, 3000],
-	FID: [100, 300],
-	LCP: [2500, 4000],
+const vitalThresholds = {
+	CLS: [ 0.1, 0.25 ],
+	FCP: [ 1800, 3000 ],
+	FID: [ 100, 300 ],
+	LCP: [ 2500, 4000 ],
 };
 
-function getRating(value, thresholds) {
-	if (value > thresholds[1]) {
-		return "poor";
+function getRating( value, thresholds ) {
+	if ( value > thresholds[ 1 ] ) {
+		return 'poor';
 	}
-	if (value > thresholds[0]) {
-		return "ni";
+	if ( value > thresholds[ 0 ] ) {
+		return 'ni';
 	}
-	return "good";
+	return 'good';
 }
 
-function getNodePath(node) {
+function getNodePath( node ) {
 	try {
 		let name = node.nodeName.toLowerCase();
-		if (name === "body") {
-			return "html>body";
+		if ( name === 'body' ) {
+			return 'html>body';
 		}
-		if (node.id) {
-			return `${name}#${node.id}`;
+		if ( node.id ) {
+			return `${ name }#${ node.id }`;
 		}
-		if (node.className && node.className.length) {
-			name += `.${[...node.classList.values()].join(".")}`;
+		if ( node.className && node.className.length ) {
+			name += `.${ [ ...node.classList.values() ].join( '.' ) }`;
 		}
-		return `${getNodePath(node.parentElement)}>${name}`;
-	} catch (error) {
-		return "(error)";
+		return `${ getNodePath( node.parentElement ) }>${ name }`;
+	} catch ( error ) {
+		return '(error)';
 	}
 }
 
-function getDebugInfo(name, entries = []) {
-	const firstEntry = entries[0];
-	const lastEntry = entries[entries.length - 1];
+function getDebugInfo( metricName, entries = [] ) {
+	const firstEntry = entries[ 0 ];
+	const lastEntry = entries[ entries.length - 1 ];
 
-	switch (name) {
-		case "LCP":
-			if (lastEntry) {
-				return getNodePath(lastEntry.element);
+	switch ( metricName ) {
+		case 'LCP':
+			if ( lastEntry ) {
+				return getNodePath( lastEntry.element );
 			}
-		case "FID":
-			if (firstEntry) {
+			break;
+		case 'FID':
+			if ( firstEntry ) {
 				const { name } = firstEntry;
 				// Report interactions with the `google-signin2` element as that,
 				// not any of the sub-elements.
-				if (firstEntry.target.closest("#google-signin2")) {
-					return `${name}(#google-signin2)`;
+				if ( firstEntry.target.closest( '#google-signin2' ) ) {
+					return `${ name }(#google-signin2)`;
 				}
-				return `${name}(${getNodePath(firstEntry.target)})`;
+				return `${ name }(${ getNodePath( firstEntry.target ) })`;
 			}
-		case "CLS":
-			if (entries.length) {
-				const largestShift = entries.reduce((a, b) => {
+			break;
+		case 'CLS':
+			if ( entries.length ) {
+				const largestShift = entries.reduce( ( a, b ) => {
 					return a && a.value > b.value ? a : b;
-				});
-				if (largestShift && largestShift.sources) {
-					const largestSource = largestShift.sources.reduce((a, b) => {
+				} );
+				if ( largestShift && largestShift.sources ) {
+					const largestSource = largestShift.sources.reduce( ( a, b ) => {
 						return a.node &&
 							a.previousRect.width * a.previousRect.height >
 								b.previousRect.width * b.previousRect.height
 							? a
 							: b;
-					});
-					if (largestSource) {
-						return getNodePath(largestSource.node);
+					} );
+					if ( largestSource ) {
+						return getNodePath( largestSource.node );
 					}
 				}
 			}
+			break;
 		default:
-			return "(not set)";
+			return '(not set)';
 	}
 }
 
-function sendToGoogleAnalytics({ name, value, delta, id, entries }) {
-	gtag("event", name, {
-		value: Math.round(name === "CLS" ? delta * 1000 : delta),
-		event_category: "Web Vitals",
+function sendToGoogleAnalytics( { name, value, delta, id, entries } ) {
+	gtag( 'event', name, {
+		value: Math.round( name === 'CLS' ? delta * 1000 : delta ),
+		event_category: 'Web Vitals',
 		event_label: id,
-		event_meta: getRating(value, thresholds[name]),
-		event_debug: getDebugInfo(name, entries),
+		event_meta: getRating( value, vitalThresholds[ name ] ),
+		event_debug: getDebugInfo( name, entries ),
 		non_interaction: true,
-	});
+	} );
 }
 
 export function measureWebVitals() {
-	getCLS(sendToGoogleAnalytics);
-	getFCP(sendToGoogleAnalytics);
-	getFID(sendToGoogleAnalytics);
-	getLCP(sendToGoogleAnalytics);
+	getCLS( sendToGoogleAnalytics );
+	getFCP( sendToGoogleAnalytics );
+	getFID( sendToGoogleAnalytics );
+	getLCP( sendToGoogleAnalytics );
 }
 
-function anonymizeConfig(state) {
-	const opts = state[`opts:${state.viewId}`];
-	if (opts && opts.active) {
+function anonymizeConfig( state ) {
+	const opts = state[ `opts:${ state.viewId }` ];
+	if ( opts && opts.active ) {
 		return [
-			`id=${opts.metricIdDim}`,
-			`name=${opts.metricNameDim}`,
-			`metrics=${[opts.lcpName, opts.fidName, opts.clsName].join(",")}`,
-			`filters=${opts.filters}`,
-		].join("|");
+			`id=${ opts.metricIdDim }`,
+			`name=${ opts.metricNameDim }`,
+			`metrics=${ [ opts.lcpName, opts.fidName, opts.clsName ].join( ',' ) }`,
+			`filters=${ opts.filters }`,
+		].join( '|' );
 	}
-	return "(not set)";
+	return '(not set)';
 }
 
-export function measureReport({ state, duration, report, error }) {
-	gtag("event", `report_${error ? "error" : "success"}`, {
+export function measureReport( { state, duration, report, error } ) {
+	gtag( 'event', `report_${ error ? 'error' : 'success' }`, {
 		value: duration,
 		report_size: report ? report.rows.length : 0,
-		segments: [state.segmentA, state.segmentB].sort().join(", "),
-		config: anonymizeConfig(state),
-		event_category: "Usage",
-		event_label: error ? error.code || error.message : "(not set)",
-		event_meta: report ? report.meta.source : "(not set)",
-	});
+		segments: [ state.segmentA, state.segmentB ].sort().join( ', ' ),
+		config: anonymizeConfig( state ),
+		event_category: 'Usage',
+		event_label: error ? error.code || error.message : '(not set)',
+		event_meta: report ? report.meta.source : '(not set)',
+	} );
 }
 
 export function initAnalytics() {
-	if ( 'undefined' === typeof( window.gtag ) ) {
+	if ( 'undefined' === typeof window.gtag ) {
+		// eslint-disable-next-line no-console
 		window.gtag = console.log;
 	}
 
-	gtag("js", new Date());
-	gtag(...getConfig("UA-XXXXXXXX-Y"));
+	gtag( 'js', new Date() );
+	gtag( ...getConfig( 'UA-XXXXXXXX-Y' ) );
 
 	measureWebVitals();
 }
@@ -180,13 +184,12 @@ export function initAnalytics() {
 ( function () {
 	if ( 'requestIdleCallback' in window ) {
 		requestIdleCallback( initAnalytics );
+	} else if ( 'complete' === document.readyState ) {
+		setTimeout( initAnalytics, 5000 );
 	} else {
-		if ( 'complete' === document.readyState ) {
+		// eslint-disable-next-line @wordpress/no-global-event-listener
+		window.addEventListener( 'load', () => {
 			setTimeout( initAnalytics, 5000 );
-		} else {
-			window.addEventListener( 'load', () => {
-				setTimeout( initAnalytics, 5000 );
-			} );
-		}
+		} );
 	}
 } )();
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const WebpackBar = require( 'webpackbar' );
 const config = {
 	srcDir: '/js/src/',
 	distDirModern: 'js/dist/module/',
-	distDirLegacy: 'js/dist/nomodule/'
+	distDirLegacy: 'js/dist/nomodule/',
 };
 
 config.modernJsEntries = {
@@ -46,50 +46,33 @@ const sharedConfig = {
 	plugins: [ ...defaultConfig.plugins ],
 };
 
-const configureBabelLoader = ( browserlist ) => {
+const configureBabelLoader = browserlist => {
 	return {
 		test: /\.js$/,
 		use: {
 			loader: 'babel-loader',
 			options: {
 				babelrc: false,
-				exclude: [
-					/core-js/,
-					/regenerator-runtime/,
-				],
+				exclude: [ /core-js/, /regenerator-runtime/ ],
 				presets: [
-					[ '@babel/preset-env', {
-						loose: true,
-						modules: false,
-						// debug: true,
-						corejs: 3,
-						useBuiltIns: 'usage',
-						targets: {
-							browsers: browserlist,
+					[
+						'@babel/preset-env',
+						{
+							loose: true,
+							modules: false,
+							// debug: true,
+							corejs: 3,
+							useBuiltIns: 'usage',
+							targets: {
+								browsers: browserlist,
+							},
 						},
-					} ],
+					],
 				],
 				plugins: [ '@babel/plugin-syntax-dynamic-import' ],
 			},
 		},
 	};
-};
-
-const legacyConfig = {
-	output: {
-		path: path.join( __dirname, config.distDirLegacy ),
-		filename: `[name].js`,
-		chunkFilename: `[name].js`,
-	},
-	module: {
-		rules: [
-			configureBabelLoader( [
-				'> 2%',
-				'not IE 11',
-				'last 2 versions',
-			] ),
-		],
-	},
 };
 
 const modernConfig = {
@@ -103,29 +86,19 @@ const modernConfig = {
 			configureBabelLoader( [
 				// The last two versions of each browser, excluding versions
 				// that don't support <script type="module">.
-				'last 2 Chrome versions', 'not Chrome < 60',
-				'last 2 Safari versions', 'not Safari < 10.1',
-				'last 2 iOS versions', 'not iOS < 10.3',
-				'last 2 Firefox versions', 'not Firefox < 54',
-				'last 2 Edge versions', 'not Edge < 15',
+				'last 2 Chrome versions',
+				'not Chrome < 60',
+				'last 2 Safari versions',
+				'not Safari < 10.1',
+				'last 2 iOS versions',
+				'not iOS < 10.3',
+				'last 2 Firefox versions',
+				'not Firefox < 54',
+				'last 2 Edge versions',
+				'not Edge < 15',
 			] ),
 		],
 	},
-};
-
-const legacyJs = {
-	...defaultConfig,
-	...sharedConfig,
-	...legacyConfig,
-	entry: config.legacyJsEntries,
-	plugins: [
-		...sharedConfig.plugins,
-		// Display nice progress bar while building or watching.
-		new WebpackBar( {
-			name: 'LegacyJs',
-			color: '#003296',
-		} ),
-	],
 };
 
 const modernJs = {
