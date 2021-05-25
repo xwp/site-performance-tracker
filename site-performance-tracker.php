44f380edbd9b98658bc9379d1e76fc3db85155bd
Piotr Bajer <piotr.bajer@xwp.co>
2021-05-25 12:16:28 +0200

    Update changelog, bump version

--- site-performance-tracker.php
+++ site-performance-tracker.php
@@ -8,7 +8,7 @@
  * Plugin Name: Site Performance Tracker
  * Plugin URI: https://github.com/xwp/site-performance-tracker
  * Description: Allows you to detect and track site performance metrics.
- * Version: 0.5
+ * Version: 0.6
  * Author: XWP.co
  * Author URI: https://xwp.co
  */
