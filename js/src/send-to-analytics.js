e9216c24708d97a666fc409e4bc029fa7f24105a
Mike Crantea <mike.crantea@xwp.co>
2021-10-04 13:54:27 +0100

    Analytics -> Instantiate using array first element of custom data

--- js/src/send-to-analytics.js
+++ js/src/send-to-analytics.js
@@ -126,10 +126,11 @@ function getDebugInfo( metricName, entries = [] ) {
 
 export function sendToAnalytics( { name, value, delta, id, entries } ) {
 	let gtagConfigured = false;
+	const analyticsData = window.webVitalsAnalyticsData[0];
 
-	if ( window.webVitalsAnalyticsData.gtag_id ) {
+	if ( analyticsData.gtag_id ) {
 		if ( ! gtagConfigured ) {
-			configureGtag( window.webVitalsAnalyticsData.gtag_id );
+			configureGtag( analyticsData.gtag_id );
 			gtagConfigured = true;
 		}
 
@@ -142,7 +143,7 @@ export function sendToAnalytics( { name, value, delta, id, entries } ) {
 			event_debug: getDebugInfo( name, entries ),
 		} );
 	}
-	if ( window.webVitalsAnalyticsData.ga_id ) {
+	if ( analyticsData.ga_id ) {
 		getDeliveryFunction( 'ga' )( 'send', 'event', {
 			eventCategory: 'Web Vitals',
 			eventAction: name,
@@ -155,7 +156,7 @@ export function sendToAnalytics( { name, value, delta, id, entries } ) {
 			[ uaDimMeasurementVersion ]: measurementVersion,
 		} );
 	}
-	if ( window.webVitalsAnalyticsData.ga4_id ) {
+	if ( analyticsData.ga4_id ) {
 		getDeliveryFunction( 'gtag' )( 'event', name, {
 			value: delta,
 			metric_id: id,
