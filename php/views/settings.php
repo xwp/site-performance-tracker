9b1ae65910250289da7043b41f1d679ce61052d4
ldanielgiuliani <ldanielgiuliani@gmail.com>
2021-11-23 13:51:23 +0800

    Run phpcbf

--- php/views/settings.php
+++ php/views/settings.php
@@ -9,7 +9,7 @@ use XWP\Site_Performance_Tracker\Plugin;
 
 /**
  * Get available trackers and print 'readonly' in the form inputs.
- * 
+ *
  * @return string|null "readonly" or null.
  */
 function print_readonly() {
@@ -32,7 +32,7 @@ add_action( 'admin_init', 'spt_settings_init' );
 /**
  * Add tracker as a settings menu item.
  */
-function spt_add_admin_menu() { 
+function spt_add_admin_menu() {
 	add_options_page( 'Site Performance Tracker', 'Site Performance Tracker', 'manage_options', 'site_performance_tracker', 'spt_options_page' );
 }
 
@@ -40,69 +40,69 @@ function spt_add_admin_menu() {
  * Initialize tracker settings by registering it and adding
  * sections and fields.
  */
-function spt_settings_init() { 
+function spt_settings_init() {
 	register_setting( 'pluginPage', 'spt_settings' );
 
 	add_settings_section(
-		'spt_pluginPage_section', 
-		null, 
-		'spt_settings_section_callback', 
+		'spt_pluginPage_section',
+		null,
+		'spt_settings_section_callback',
 		'pluginPage'
 	);
 
-	add_settings_field( 
-		'analytics_types', 
-		__( 'Analytics Types', 'site-performance-tracker' ), 
-		'analytics_types_render', 
-		'pluginPage', 
-		'spt_pluginPage_section' 
+	add_settings_field(
+		'analytics_types',
+		__( 'Analytics Types', 'site-performance-tracker' ),
+		'analytics_types_render',
+		'pluginPage',
+		'spt_pluginPage_section'
 	);
 
-	add_settings_field( 
-		'analytics_id', 
-		__( 'Analytics ID', 'site-performance-tracker' ), 
-		'analytics_id_render', 
-		'pluginPage', 
-		'spt_pluginPage_section' 
+	add_settings_field(
+		'analytics_id',
+		__( 'Analytics ID', 'site-performance-tracker' ),
+		'analytics_id_render',
+		'pluginPage',
+		'spt_pluginPage_section'
 	);
 
-	add_settings_field( 
-		'measurement_version_dimension', 
-		__( 'Measurement Version Dimension', 'site-performance-tracker' ), 
-		'measurement_version_dimension_render', 
-		'pluginPage', 
-		'spt_pluginPage_section' 
+	add_settings_field(
+		'measurement_version_dimension',
+		__( 'Measurement Version Dimension', 'site-performance-tracker' ),
+		'measurement_version_dimension_render',
+		'pluginPage',
+		'spt_pluginPage_section'
 	);
 
-	add_settings_field( 
-		'event_meta_dimension', 
-		__( 'Event Meta Dimension', 'site-performance-tracker' ), 
-		'event_meta_dimension_render', 
-		'pluginPage', 
-		'spt_pluginPage_section' 
+	add_settings_field(
+		'event_meta_dimension',
+		__( 'Event Meta Dimension', 'site-performance-tracker' ),
+		'event_meta_dimension_render',
+		'pluginPage',
+		'spt_pluginPage_section'
 	);
 
-	add_settings_field( 
-		'event_debug_dimension', 
-		__( 'Event Debug Dimension', 'site-performance-tracker' ), 
-		'event_debug_dimension_render', 
-		'pluginPage', 
-		'spt_pluginPage_section' 
+	add_settings_field(
+		'event_debug_dimension',
+		__( 'Event Debug Dimension', 'site-performance-tracker' ),
+		'event_debug_dimension_render',
+		'pluginPage',
+		'spt_pluginPage_section'
 	);
 
-	add_settings_field( 
-		'web_vitals_tracking_ratio', 
-		__( 'Web Vitals Tracking Ratio', 'site-performance-tracker' ), 
-		'web_vitals_tracking_ratio_render', 
-		'pluginPage', 
-		'spt_pluginPage_section' 
+	add_settings_field(
+		'web_vitals_tracking_ratio',
+		__( 'Web Vitals Tracking Ratio', 'site-performance-tracker' ),
+		'web_vitals_tracking_ratio_render',
+		'pluginPage',
+		'spt_pluginPage_section'
 	);
 }
 
 /**
  * Render Analytics Types form dropdown.
  */
-function analytics_types_render() { 
+function analytics_types_render() {
 	$options = get_option( 'spt_settings' );
 	?>
 	<select name='spt_settings[analytics_types]' required>
@@ -116,7 +116,7 @@ function analytics_types_render() {
 /**
  * Render Analytics ID form input.
  */
-function analytics_id_render() { 
+function analytics_id_render() {
 	$options = get_option( 'spt_settings' );
 	?>
 	<input type='text' name='spt_settings[analytics_id]' pattern="[UA|GTM|G]+-[A-Z|0-9]+.*" value='<?php echo esc_attr( $options['analytics_id'] ); ?>' placeholder="UA-XXXXXXXX-Y" aria-label="analytics id" <?php print_readonly(); ?> required>
@@ -126,7 +126,7 @@ function analytics_id_render() {
 /**
  * Render Measurement Version Dimension form input.
  */
-function measurement_version_dimension_render() { 
+function measurement_version_dimension_render() {
 	$options = get_option( 'spt_settings' );
 	?>
 	<input type='text' name='spt_settings[measurement_version_dimension]' pattern="[dimension]+[0-9]{1,2}" value='<?php echo esc_attr( $options['measurement_version_dimension'] ); ?>' placeholder="dimension1" aria-label="measurement version dimension" <?php print_readonly(); ?> required>
@@ -136,7 +136,7 @@ function measurement_version_dimension_render() {
 /**
  * Render Event Meta Dimension form input.
  */
-function event_meta_dimension_render() { 
+function event_meta_dimension_render() {
 	$options = get_option( 'spt_settings' );
 	?>
 	<input type='text' name='spt_settings[event_meta_dimension]' pattern="[dimension]+[0-9]{1,2}" value='<?php echo esc_attr( $options['event_meta_dimension'] ); ?>' placeholder="dimension2" aria-label="event meta dimension" <?php print_readonly(); ?> required>
@@ -146,7 +146,7 @@ function event_meta_dimension_render() {
 /**
  * Render Event Debug Dimension form input.
  */
-function event_debug_dimension_render() { 
+function event_debug_dimension_render() {
 	$options = get_option( 'spt_settings' );
 	?>
 	<input type='text' name='spt_settings[event_debug_dimension]' pattern="[dimension]+[0-9]{1,2}" value='<?php echo esc_attr( $options['event_debug_dimension'] ); ?>' placeholder="dimension3" aria-label="event debug dimension" <?php print_readonly(); ?> required>
@@ -156,7 +156,7 @@ function event_debug_dimension_render() {
 /**
  * Render Tracking Ratio form input.
  */
-function web_vitals_tracking_ratio_render() { 
+function web_vitals_tracking_ratio_render() {
 	$options = get_option( 'spt_settings' );
 	?>
 	<input type='number' name='spt_settings[web_vitals_tracking_ratio]' step='0.01' value='0.00' min='0.01' max='1' value='<?php echo esc_attr( $options['web_vitals_tracking_ratio'] ); ?>' placeholder="Enter between 0 > 1" aria-label="web vitals tracking ratio" <?php print_readonly(); ?> required>
@@ -166,14 +166,14 @@ function web_vitals_tracking_ratio_render() {
 /**
  * Echo section callback text.
  */
-function spt_settings_section_callback() { 
+function spt_settings_section_callback() {
 	echo esc_html( __( 'Update Site Performance Tracker settings', 'site-performance-tracker' ) );
 }
 
 /**
  * Create and Output the form.
  */
-function spt_options_page() { 
+function spt_options_page() {
 	?>
 	<form action='options.php' method='post'>
 		<h1><?php echo esc_html( __( 'Site Performance Tracker Settings', 'site-performance-tracker' ) ); ?></h1>
