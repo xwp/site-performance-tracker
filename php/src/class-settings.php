a1dd5dbb3b52480975234ea5e7f963e24304137f
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2022-06-09 15:05:57 +0300

    Move init settings code without callbacks

--- php/src/class-settings.php
+++ php/src/class-settings.php
@@ -24,6 +24,7 @@ class Settings {
 	 */
 	protected function register_hooks() {
 		add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+		add_action( 'admin_init', array( $this, 'settings_init' ) );
 	}
 
 	/**
@@ -31,5 +32,68 @@ class Settings {
 	 */
 	public function add_admin_menu() {
 		add_options_page( 'Site Performance Tracker', 'Site Performance Tracker', 'manage_options', 'site_performance_tracker', 'spt_options_page' );
+	}
+
+	/**
+	 * Initialize tracker settings by registering it and adding
+	 * sections and fields.
+	 */
+	public function settings_init() {
+		register_setting( 'pluginPage', 'spt_settings' );
+
+		add_settings_section(
+			'spt_pluginPage_section',
+			null,
+			'spt_settings_section_callback',
+			'pluginPage'
+		);
+
+		add_settings_field(
+			'analytics_types',
+			__( 'Analytics Types', 'site-performance-tracker' ),
+			'analytics_types_render',
+			'pluginPage',
+			'spt_pluginPage_section'
+		);
+
+		add_settings_field(
+			'analytics_id',
+			__( 'Analytics ID', 'site-performance-tracker' ),
+			'analytics_id_render',
+			'pluginPage',
+			'spt_pluginPage_section'
+		);
+
+		add_settings_field(
+			'measurement_version_dimension',
+			__( 'Measurement Version Dimension', 'site-performance-tracker' ),
+			'measurement_version_dimension_render',
+			'pluginPage',
+			'spt_pluginPage_section'
+		);
+
+		add_settings_field(
+			'event_meta_dimension',
+			__( 'Event Meta Dimension', 'site-performance-tracker' ),
+			'event_meta_dimension_render',
+			'pluginPage',
+			'spt_pluginPage_section'
+		);
+
+		add_settings_field(
+			'event_debug_dimension',
+			__( 'Event Debug Dimension', 'site-performance-tracker' ),
+			'event_debug_dimension_render',
+			'pluginPage',
+			'spt_pluginPage_section'
+		);
+
+		add_settings_field(
+			'web_vitals_tracking_ratio',
+			__( 'Web Vitals Tracking Ratio', 'site-performance-tracker' ),
+			'web_vitals_tracking_ratio_render',
+			'pluginPage',
+			'spt_pluginPage_section'
+		);
 	}
 }
