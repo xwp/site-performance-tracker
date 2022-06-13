bc6f7bb23c61ca682d6bb6c8b69a0a2b498099de
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2022-06-13 17:36:42 +0300

    Added unit test for get_hardcoded tracker_config with empty theme

--- tests/phpunit/class-test-settings.php
+++ tests/phpunit/class-test-settings.php
@@ -12,6 +12,13 @@ class Test_Settings extends WP_UnitTestCase {
 		$this->settings = new Settings();
 	}
 
+	function tearDown(): void {
+		parent::tearDown();
+
+		global $tracker_config;
+		$tracker_config = null;
+	}
+
 	public function test_init_register_actions() {
 		$this->assertFalse( has_action( 'after_setup_theme', array( $this->settings, 'get_hardcoded_tracker_config' ) ) );
 		$this->assertFalse( has_action( 'admin_menu', array( $this->settings, 'add_admin_menu' ) ) );
@@ -22,5 +29,16 @@ class Test_Settings extends WP_UnitTestCase {
 		$this->assertEquals( PHP_INT_MAX, has_action( 'after_setup_theme', array( $this->settings, 'get_hardcoded_tracker_config' ) ) );
 		$this->assertNotFalse( has_action( 'admin_menu', array( $this->settings, 'add_admin_menu' ) ) );
 		$this->assertNotFalse( has_action( 'admin_init', array( $this->settings, 'settings_init' ) ) );
+	}
+
+	public function test_get_hardcoded_tracker_config__with_empty_theme() {
+		 global $tracker_config;
+
+		$this->assertNull( $tracker_config );
+
+		$this->settings->get_hardcoded_tracker_config();
+
+		$this->assertIsArray( $tracker_config );
+		$this->assertTrue( empty( $tracker_config ) );
 	}
 }
