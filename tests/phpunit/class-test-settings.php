18b9f2de133836677a7e0323c2a9b092a9bca016
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2022-06-16 02:03:25 +0300

    Implement unit test for analytics_id_render with gtag_id option set

--- tests/phpunit/class-test-settings.php
+++ tests/phpunit/class-test-settings.php
@@ -449,6 +449,23 @@ EOD;
 		$this->assertSameIgnoreEOL( $this->normilize( $expected_html ), $this->normilize( $result ) );
 	}
 
+	public function test_analytics_id_render_set_gtag_id() {
+		add_option( 'spt_settings', array( 'gtag_id' => 'test_gtag_id' ) );
+
+		ob_start();
+		$this->settings->analytics_id_render();
+		$result = ob_get_contents();
+		ob_end_clean();
+
+		$expected_html = <<<EOD
+			<input type='text' name='spt_settings[gtag_id]' pattern="[UA|GTM|G]+-[A-Z|0-9]+.*"
+				value='test_gtag_id' placeholder="UA-XXXXXXXX-Y"
+				aria-label="analytics id"  required>
+EOD;
+
+		$this->assertSameIgnoreEOL( $this->normilize( $expected_html ), $this->normilize( $result ) );
+	}
+
 	private function normilize( $str ) {
 		return trim( preg_replace( '/\s+/', ' ', $str ) );
 	}
