04ba338d065a394dfd32c4cf183172d0ba7c2f9b
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2022-06-24 15:41:28 +0300

    Extends MeasurementVersionDimensionField from FieldBase

--- php/src/class-measurementversiondimensionfield.php
+++ php/src/class-measurementversiondimensionfield.php
@@ -11,14 +11,7 @@ namespace XWP\Site_Performance_Tracker;
 /**
  * Class MeasurementVersionDimensionField
  */
-class MeasurementVersionDimensionField {
-	/**
-	 * Setting that current fields belong to
-	 *
-	 * @var Settings
-	 */
-	protected $settings;
-
+class MeasurementVersionDimensionField extends FieldBase {
 	/**
 	 * Option measurementVersion name
 	 *
@@ -27,25 +20,17 @@ class MeasurementVersionDimensionField {
 	const OPTION_MEASUREMENT_VERSION = 'measurementVersion';
 
 	/**
-	 * Initialize settings.
+	 * Get current field id
 	 */
-	/**
-	 * Initialize field.
-	 *
-	 * @param Settings $settings settings that current fields belong to.
-	 * @param string   $page_id field page id.
-	 * @param string   $section_id field section id.
-	 */
-	public function init( $settings, $page_id, $section_id ) {
-		add_settings_field(
-			'measurement_version_dimension',
-			__( 'Measurement Version Dimension', 'site-performance-tracker' ),
-			array( $this, 'render' ),
-			$page_id,
-			$section_id
-		);
+	protected function get_id() {
+		return 'measurement_version_dimension';
+	}
 
-		$this->settings = $settings;
+	/**
+	 * Get current field title
+	 */
+	protected function get_title() {
+		return __( 'Measurement Version Dimension', 'site-performance-tracker' );
 	}
 
 	/**
