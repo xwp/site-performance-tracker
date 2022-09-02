45ff73e63d4aa145410e49022d413dcca3b94bc1
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2022-09-02 19:27:22 +0300

    Update EventDebugDimensionField class: Add period to comments Extract constants Make class final Add translations

--- php/src/class-eventdebugdimensionfield.php
+++ php/src/class-eventdebugdimensionfield.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Create the Event Debug Dimension field
+ * Create the Event Debug Dimension field.
  *
  * @package XWP\Site_Performance_Tracker
  * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
@@ -9,48 +9,55 @@
 namespace XWP\Site_Performance_Tracker;
 
 /**
- * Class EventDebugDimensionField
+ * Defines behaviour for event debug dimsnsion field.
  */
-class EventDebugDimensionField extends DimensionFieldBase {
+final class EventDebugDimensionField extends DimensionFieldBase {
 
 	/**
-	 * Option eventDebug name
+	 * Option eventDebug name.
 	 *
 	 * @var string
 	 */
 	const OPTION_EVENT_DEBUG = 'eventDebug';
 
 	/**
-	 * Get current field id
+	 * Option eventDebug name.
+	 *
+	 * @var string
+	 */
+	const OPTION_EVENT_DEBUG_DIMENSION = 'event_debug_dimension';
+
+	/**
+	 * Get current field id.
 	 */
 	protected function get_id() {
-		return 'event_debug_dimension';
+		return self::OPTION_EVENT_DEBUG_DIMENSION;
 	}
 
 	/**
-	 * Get current field title
+	 * Get current field title.
 	 */
 	protected function get_title() {
 		return __( 'Event Debug Dimension', 'site-performance-tracker' );
 	}
 
 	/**
-	 * Get option name
+	 * Get option name.
 	 */
 	protected function get_option_name() {
 		return self::OPTION_EVENT_DEBUG;
 	}
 	/**
-	 * Get field placeholder
+	 * Get field placeholder.
 	 */
 	protected function get_placeholder() {
-		return 'dimension3';
+		return __( 'dimension3', 'site-performance-tracker' );
 	}
 
 	/**
-	 * Get field aria label
+	 * Get field aria label.
 	 */
 	protected function get_aria_label() {
-		return 'event debug dimension';
+		return __( 'event debug dimension', 'site-performance-tracker' );;
 	}
 }
