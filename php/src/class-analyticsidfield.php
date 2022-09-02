69717e1fa86fcf2271f3c558bfe054f3e85c84df
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2022-09-02 18:01:03 +0300

    AnalyticsIdField class make class final Extract option into constant Add period to comments Refine some comments Use more speaking names for variables

--- php/src/class-analyticsidfield.php
+++ php/src/class-analyticsidfield.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Create the Analytics ID field
+ * Create the Analytics ID field.
  *
  * @package XWP\Site_Performance_Tracker
  * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
@@ -9,25 +9,33 @@
 namespace XWP\Site_Performance_Tracker;
 
 /**
- * Class AnalyticsIdField
+ * Stores the Google Analytics ID.
  */
-class AnalyticsIdField extends FieldBase {
+final class AnalyticsIdField extends FieldBase {
 	/**
-	 * Option gtag_id name
+	 * Option gtag_id name.
 	 *
 	 * @var string
 	 */
 	const OPTION_TAG_ID = 'gtag_id';
 
+
 	/**
-	 * Get current field id
+	 * Option gtag_id name.
+	 *
+	 * @var string
+	 */
+	const OPTION_ANALYTICS_ID = 'analytics_id';
+
+	/**
+	 * Get current field id.
 	 */
 	protected function get_id() {
-		return 'analytics_id';
+		return self::OPTION_ANALYTICS_ID;
 	}
 
 	/**
-	 * Get current field title
+	 * Get current field title.
 	 */
 	protected function get_title() {
 		return __( 'Analytics ID', 'site-performance-tracker' );
@@ -39,8 +47,8 @@ class AnalyticsIdField extends FieldBase {
 	public function render() {
 		$options = $this->settings->get_settings();
 		global $tracker_config;
-		$set  = false;
-		$prop = self::OPTION_TAG_ID;
+		$display_theme_override_warning  = false;
+		$property_name = self::OPTION_TAG_ID;
 
 		if ( isset( $options['ga_id'] ) ) {
 			$options[ self::OPTION_TAG_ID ] = $options['ga_id'];
@@ -48,22 +56,22 @@ class AnalyticsIdField extends FieldBase {
 
 		if ( isset( $tracker_config['ga_id'] ) ) {
 			$options[ self::OPTION_TAG_ID ] = $tracker_config['ga_id'];
-			$prop                           = 'ga_id';
-			$set                            = true;
+			$property_name                  = 'ga_id';
+			$display_theme_override_warning = true;
 		} elseif ( isset( $tracker_config[ self::OPTION_TAG_ID ] ) ) {
 			$options[ self::OPTION_TAG_ID ] = $tracker_config[ self::OPTION_TAG_ID ];
-			$set                            = true;
+			$display_theme_override_warning                            = true;
 		} elseif ( isset( $tracker_config['ga4_id'] ) ) {
 			$options[ self::OPTION_TAG_ID ] = $tracker_config['ga4_id'];
-			$prop                           = 'ga4_id';
-			$set                            = true;
+			$property_name                  = 'ga4_id';
+			$display_theme_override_warning = true;
 		}
 		?>
 		<input type='text' name='spt_settings[<?php echo esc_attr( self::OPTION_TAG_ID ); ?>]' pattern="[UA|GTM|G]+-[A-Z|0-9]+.*"
 			   value='<?php echo esc_attr( $options[ self::OPTION_TAG_ID ] ); ?>' placeholder="UA-XXXXXXXX-Y"
-			   aria-label="analytics id" <?php $this->print_readonly( $prop ); ?> required>
+			   aria-label="analytics id" <?php $this->print_readonly( $property_name ); ?> required>
 		<?php
 
-		$this->show_theme_warning( $set );
+		$this->show_theme_warning( $display_theme_override_warning );
 	}
 }
