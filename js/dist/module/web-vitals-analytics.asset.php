8645a15129db7739424df1d2a422915cea35042d
Mike Crantea <mike.crantea@xwp.co>
2024-07-28 16:36:21 +0100

    Update changelog, version

--- js/dist/module/web-vitals-analytics.asset.php
+++ js/dist/module/web-vitals-analytics.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'e53aec45ee02272dfd68cf28c8f0a8cc');
+<?php return array('dependencies' => array(), 'version' => '26c9e8650a743f1768edb15ad2170f0f');
